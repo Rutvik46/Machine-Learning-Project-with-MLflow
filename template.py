9c5d74d3e1400169a721026c20f69b1be3f4e408
Rutvik46 <rutvikroy46@gmail.com>
2025-07-15 21:55:44 -0400

    project files created

---
+++ template.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] : %(message)s:')
+
+Project_name = "mlPorject with MLflow"
+
+list_of_files = [ 
+    ".github/workflows/.gitkeep",
+    f"src/{Project_name}/__init__.py",
+    f"src/{Project_name}/components/__init__.py",
+    f"src/{Project_name}/utils/__init__.py",
+    f"src/{Project_name}/utils/common.py",
+    f"src/{Project_name}/config/__init__.py",
+    f"src/{Project_name}/config/configuration.py",
+    f"src/{Project_name}/pipeline/__init__.py",
+    f"src/{Project_name}/entity/__init__.py",
+    f"src/{Project_name}/entity/config_entity.py",
+    f"src/{Project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    "test.py"
+
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directoy: {filedir} for the file: {filename}")
+
+    if not os.path.exists(filename) or os.path.getsize(filename):
+        with open(filepath, "w") as file:
+            pass
+            logging.info(f"Creating empty file: {filename}")
+
+    else:
+        logging.info(f"{filename} already exists")  
