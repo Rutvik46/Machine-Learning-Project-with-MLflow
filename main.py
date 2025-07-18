20e224d9b2a7e9c7f7521aa96aa019caf57c015d
Rutvik46 <rutvikroy46@gmail.com>
2025-07-18 03:24:37 -0400

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from ml_project import logger
+from ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Logging has been set up successfully.")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/ml_project/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,26 @@
+from ml_project.components.data_ingestion import DataIngestion
+from ml_project.config.configuration import ConfigrationManger
+from ml_project import logger
+
+STAGE_NAME = "Data Ingestion Stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+            config_manager = ConfigrationManger()
+            data_ingestion_config = config_manager.get_data_ingestion_config()
+            data_ingestion = DataIngestion(config=data_ingestion_config)
+            data_ingestion.download_file()
+            data_ingestion.extract_zip_file()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
